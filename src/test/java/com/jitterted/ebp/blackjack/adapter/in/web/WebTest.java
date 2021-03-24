d875db81288e86392adfe353d8b483c92c37d7cc
Ted M. Young <tedyoung@gmail.com>
2021-03-24 10:31:21 -0700

    Added home page.

---
+++ src/test/java/com/jitterted/ebp/blackjack/adapter/in/web/WebTest.java
@@ -0,0 +1,23 @@
+package com.jitterted.ebp.blackjack.adapter.in.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest
+public class WebTest {
+
+  @Autowired
+  MockMvc mockMvc;
+
+  @Test
+  public void getOfHomePageIsStatus200Ok() throws Exception {
+    mockMvc.perform(get("/index.html"))
+           .andExpect(status().isOk());
+  }
+
+}
