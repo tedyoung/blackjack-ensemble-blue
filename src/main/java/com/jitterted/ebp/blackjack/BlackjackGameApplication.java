72ae4cfd32ab5f32f3d9a34b1016e203ceb22b52
Ted M. Young <tedyoung@gmail.com>
2021-03-24 09:52:11 -0700

    Day 3: Baseline for new Spring MVC (Web) Adapter.

---
+++ src/main/java/com/jitterted/ebp/blackjack/BlackjackGameApplication.java
@@ -0,0 +1,13 @@
+package com.jitterted.ebp.blackjack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlackjackGameApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(BlackjackGameApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/jitterted/ebp/blackjack/BlackjackGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jitterted.ebp.blackjack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlackjackGameApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
