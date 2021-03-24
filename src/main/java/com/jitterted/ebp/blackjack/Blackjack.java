0b21bb63b4d0d9f7f42691f4993e018fdf904f91
Ted M. Young <tedyoung@gmail.com>
2021-03-24 09:20:26 -0700

    Fix wrong package for Blackjack "assembler" that contains the main() method for the application.

--- src/main/java/com/jitterted/ebp/blackjack/Blackjack.java
+++ src/main/java/com/jitterted/ebp/blackjack/Blackjack.java
@@ -1,9 +1,11 @@
-package com.jitterted.ebp;
+package com.jitterted.ebp.blackjack;
 
 import com.jitterted.ebp.blackjack.adapter.in.console.ConsoleGame;
 import com.jitterted.ebp.blackjack.domain.Game;
 
 public class Blackjack {
+
+  // This the Application Assembler that configures and starts the application
   public static void main(String[] args) {
     Game game = new Game();
     ConsoleGame consoleGame = new ConsoleGame(game); // in general: Entities aren't directly passed in to Adapters
