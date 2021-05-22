4dc2caa10ec970dd875f7385a2229bd400f9b939
manoj <gzmodesgns@gmail.com>
2021-05-22 13:48:10 -0400

    mob next [ci-skip] [ci skip] [skip ci]

--- src/main/java/com/jitterted/ebp/blackjack/adapter/out/repository/GameOutcomeDto.java
+++ src/main/java/com/jitterted/ebp/blackjack/adapter/out/repository/GameOutcomeDto.java
@@ -1,4 +1,15 @@
 package com.jitterted.ebp.blackjack.adapter.out.repository;
 
+import com.jitterted.ebp.blackjack.domain.Game;
+
 public class GameOutcomeDto {
+    private Game game;
+
+    public GameOutcomeDto(Game game) {
+        this.game = game;
+    }
+
+    public String asString() {
+        return null;
+    }
 }
--- src/test/java/com/jitterted/ebp/blackjack/adapter/out/repository/GameOutcomeDtoTest.java
+++ src/test/java/com/jitterted/ebp/blackjack/adapter/out/repository/GameOutcomeDtoTest.java
@@ -1,7 +1,6 @@
 package com.jitterted.ebp.blackjack.adapter.out.repository;
 
 import com.jitterted.ebp.blackjack.domain.*;
-import jdk.internal.net.http.frame.Http2Frame;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
@@ -11,13 +10,11 @@ import static org.assertj.core.api.Assertions.*;
 public class GameOutcomeDtoTest {
   @Test
   public void playerStandsGameIsOverAsString() throws Exception {
-    Game game = new Game(new StubDeck(List.of(new Card(Suit.HEARTS, Rank.QUEEN),
-                                              new Card(Suit.HEARTS, Rank.THREE),
-                                              new Card(Suit.HEARTS, Rank.EIGHT),
-                                              new Card(Suit.HEARTS, Rank.FIVE),
-                                              new Card(Suit.HEARTS, Rank.JACK)
-    )));
-
+    Game game = new Game(new StubDeck(List.of(new Card(Suit.HEARTS, Rank.QUEEN), new Card(Suit.HEARTS, Rank.THREE),
+                                              new Card(Suit.HEARTS, Rank.EIGHT), new Card(Suit.HEARTS, Rank.FIVE),
+                                                                                 new Card(Suit.HEARTS, Rank.JACK))));
+    game.initialDeal();
+    game.playerStands();
 
     GameOutcomeDto gameOutcomeDto = new GameOutcomeDto(game);
     String gameAsString = gameOutcomeDto.asString();
