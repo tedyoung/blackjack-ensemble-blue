5cf84fb4aa2dfb9f47993b6b2b6292cf4b763dd7
Ted M. Young <tedyoung@gmail.com>
2021-12-03 14:40:45 -0800

    Ensemble #37
    Completed PlayerOutcomeView and now needs some Refactoring 💙

--- src/main/java/com/jitterted/ebp/blackjack/adapter/in/web/PlayerOutcomeView.java
+++ src/main/java/com/jitterted/ebp/blackjack/adapter/in/web/PlayerOutcomeView.java
@@ -1,12 +1,47 @@
 package com.jitterted.ebp.blackjack.adapter.in.web;
 
+import com.jitterted.ebp.blackjack.domain.Card;
+import com.jitterted.ebp.blackjack.domain.Hand;
 import com.jitterted.ebp.blackjack.domain.Player;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class PlayerOutcomeView {
 
+    private int playerId;
+    private List<String> playerCards;
     private Player player;
+    private Hand dealerHand;
 
-    public PlayerOutcomeView(Player player) {
-        this.player = player;
+    public static PlayerOutcomeView of(Hand dealerBustedHand, Player player) {
+        PlayerOutcomeView playerOutcomeView = new PlayerOutcomeView();
+        playerOutcomeView.playerId = player.id();
+        playerOutcomeView.playerCards = cardsAsString(player.cards());
+        playerOutcomeView.player = player;
+        playerOutcomeView.dealerHand = dealerBustedHand;
+        return playerOutcomeView;
+    }
+
+    public int getPlayerId() {
+        return playerId;
+    }
+
+    public List<String> getPlayerCards() {
+        return playerCards;
+    }
+
+    private static List<String> cardsAsString(List<Card> cards) {
+        return cards.stream()
+                    .map(PlayerOutcomeView::displayOf)
+                    .collect(Collectors.toList());
+    }
+
+    private static String displayOf(Card card) {
+        return card.rank().display() + card.suit().symbol();
+    }
+
+    public String getPlayerOutcome() {
+        return player.outcome(dealerHand).toString();
     }
 }
--- src/test/java/com/jitterted/ebp/blackjack/adapter/in/web/PlayerOutcomeViewTest.java
+++ src/test/java/com/jitterted/ebp/blackjack/adapter/in/web/PlayerOutcomeViewTest.java
@@ -1,30 +1,54 @@
 package com.jitterted.ebp.blackjack.adapter.in.web;
 
+import com.jitterted.ebp.blackjack.domain.Card;
 import com.jitterted.ebp.blackjack.domain.Deck;
+import com.jitterted.ebp.blackjack.domain.Hand;
 import com.jitterted.ebp.blackjack.domain.Player;
-import com.jitterted.ebp.blackjack.domain.SinglePlayerStubDeckFactory;
+import com.jitterted.ebp.blackjack.domain.Rank;
+import com.jitterted.ebp.blackjack.domain.StubDeck;
+import com.jitterted.ebp.blackjack.domain.Suit;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
 
 class PlayerOutcomeViewTest {
 
     @Test
     public void playerHasBlackjackThenDisplaysIdCardsAndOutcome() throws Exception {
-        // PlayerOutcomeView
-            // PlayerId
-            // List<String> playerCards
-            // PlayerOutcome
-
-        Deck deck = SinglePlayerStubDeckFactory.createPlayerDealtBlackjackDeckAndDealerCanNotHit();
-        Player player = new Player();
+        Deck deck = new StubDeck(Rank.KING, Rank.ACE);
+        Player player = new Player(1);
+        player.initialDrawFrom(deck);
         player.initialDrawFrom(deck);
 
-        PlayerOutcomeView playerOutcomeView = new PlayerOutcomeView(player);
+        PlayerOutcomeView playerOutcomeView = PlayerOutcomeView.of(null, player);
 
         assertThat(playerOutcomeView.getPlayerId())
                 .isEqualTo(1);
         assertThat(playerOutcomeView.getPlayerCards())
-                .hasSize(2);
+                .hasSize(2)
+                .containsOnly("K♥", "A♥");
         assertThat(playerOutcomeView.getPlayerOutcome())
                 .isEqualTo("BLACKJACK");
     }
+
+    @Test
+    public void dealerBustedAndPlayerNotBustedThenOutcomeIsDealerBusted() throws Exception {
+        Hand dealerBustedHand = new Hand(List.of(new Card(Suit.CLUBS, Rank.TEN),
+                                                 new Card(Suit.CLUBS, Rank.QUEEN),
+                                                 new Card(Suit.CLUBS, Rank.TWO)
+        ));
+        Deck deck = new StubDeck(Rank.KING, Rank.EIGHT);
+        Player player = new Player(1);
+        player.initialDrawFrom(deck);
+        player.initialDrawFrom(deck);
+        player.stand();
+
+        PlayerOutcomeView playerOutcomeView = PlayerOutcomeView.of(dealerBustedHand, player);
+
+        assertThat(playerOutcomeView.getPlayerOutcome())
+                .isEqualTo("DEALER_BUSTED");
+    }
+
 }
