8e37eb8cadd5ede613e86a752141d472e5ddc64d
Ted M. Young <tedyoung@gmail.com>
2022-03-04 10:49:02 -0800

    Ensemble #49: Expanded test as documentation in StubDeckBuilderTest.
    Explored options for fixing Web UI: should not show Dealer's "hole" (hidden) card.

--- src/test/java/com/jitterted/ebp/blackjack/domain/StubDeckBuilderTest.java
+++ src/test/java/com/jitterted/ebp/blackjack/domain/StubDeckBuilderTest.java
@@ -6,6 +6,55 @@ import static org.assertj.core.api.Assertions.*;
 
 public class StubDeckBuilderTest {
 
+    /**
+     * Full exercise of builder, shows how the deck is built internally
+     */
+    @Test
+    public void createTwoPlayerAndDealerEachWithFourCards() {
+        StubDeck stubDeck = StubDeckBuilder
+                .playerCountOf(2)
+                .addPlayerWithRanks(Rank.TWO, Rank.THREE, Rank.JACK, Rank.TWO)
+                .addPlayerWithRanks(Rank.FIVE, Rank.SIX, Rank.FOUR, Rank.FIVE)
+                .buildWithDealerRanks(Rank.FOUR, Rank.SEVEN, Rank.FOUR, Rank.KING);
+        // @formatter:off
+        assertThat(stubDeck)
+                .isEqualTo(new StubDeck(// initial deal (2 cards for everyone)
+                                        Rank.TWO,   Rank.FIVE,  Rank.FOUR,
+                                        Rank.THREE, Rank.SIX,   Rank.SEVEN,
+                                        // player 1's turn ("tail"), hits twice:
+                                        Rank.JACK, Rank.TWO,
+                                        // player 2's turn ("tail"), hits twice:
+                                        Rank.FOUR, Rank.FIVE,
+                                        // dealer gets the rest:
+                                        Rank.FOUR, Rank.KING));
+        // @formatter:on
+        Game game = new Game(stubDeck, 2);
+        game.initialDeal();
+
+        // player 1 plays
+        game.playerHits();
+        game.playerHits();
+        game.playerStands();
+
+        // player 2 plays
+        game.playerHits();
+        game.playerHits();
+        game.playerStands();
+
+        assertThat(game.isGameOver())
+                .isTrue();
+
+        assertThat(game.playerResults().get(0).cards())
+                .extracting(Card::rank)
+                .containsExactly(Rank.TWO, Rank.THREE, Rank.JACK, Rank.TWO);
+        assertThat(game.playerResults().get(1).cards())
+                .extracting(Card::rank)
+                .containsExactly(Rank.FIVE, Rank.SIX, Rank.FOUR, Rank.FIVE);
+        assertThat(game.dealerHand().cards())
+                .extracting(Card::rank)
+                .containsExactly(Rank.FOUR, Rank.SEVEN, Rank.FOUR, Rank.KING);
+    }
+
     @Test
     public void createSinglePlayerHitsDoesNotBust() {
         StubDeckBuilder builder = StubDeckBuilder.playerCountOf(1);
@@ -42,23 +91,6 @@ public class StubDeckBuilderTest {
                 .isEqualTo(new StubDeck(Rank.QUEEN, Rank.QUEEN, Rank.EIGHT,
                                         Rank.ACE,   Rank.SEVEN, Rank.TEN,
                                                     Rank.THREE));
-        // @formatter:on
-    }
-
-    @Test
-    public void createTwoPlayerAndDealerEachWithFourCards() {
-        StubDeck stubDeck = StubDeckBuilder
-                .playerCountOf(2)
-                .addPlayerWithRanks(Rank.TWO, Rank.THREE, Rank.JACK, Rank.TWO)
-                .addPlayerWithRanks(Rank.FIVE, Rank.SIX, Rank.QUEEN, Rank.FIVE)
-                .buildWithDealerRanks(Rank.FOUR, Rank.SEVEN, Rank.KING, Rank.FOUR);
-        // @formatter:off
-        assertThat(stubDeck)
-                .isEqualTo(new StubDeck(Rank.TWO,   Rank.FIVE,  Rank.FOUR,
-                                        Rank.THREE, Rank.SIX,   Rank.SEVEN,
-                                        Rank.JACK, Rank.TWO,
-                                        Rank.QUEEN, Rank.FIVE,
-                                        Rank.KING, Rank.FOUR));
         // @formatter:on
     }
 
