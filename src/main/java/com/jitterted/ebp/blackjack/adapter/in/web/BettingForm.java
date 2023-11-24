2a42daef09d31366e673e9f8800e432a63f84c82
Soumya Kulkarni <soumya.kulkarni25@t-mobile.com>
2023-11-24 09:59:31 -0800

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:src/main/java/com/jitterted/ebp/blackjack/adapter/in/web/BettingForm.java

--- src/main/java/com/jitterted/ebp/blackjack/adapter/in/web/BettingForm.java
+++ src/main/java/com/jitterted/ebp/blackjack/adapter/in/web/BettingForm.java
@@ -13,20 +13,10 @@ import java.util.Map;
 import java.util.Objects;
 
 public class BettingForm {
-    @Deprecated // the new HTML form uses the Map instead of separate lists
-    private List<Integer> playerIds = new ArrayList<>();
-
-    @Deprecated // the new HTML form uses the Map instead of separate lists
-    private List<Integer> bets = new ArrayList<>();
 
     private Map<String, String> playerIdToBets;
 
     public BettingForm() {
-    }
-
-    @Deprecated
-    public BettingForm(List<Integer> bets) {
-        this.bets = bets;
     }
 
     public BettingForm(Map<String, String> playerIdToBets) {
@@ -41,11 +31,6 @@ public class BettingForm {
         return new BettingForm(playerBets);
     }
 
-    @Deprecated
-    public List<Integer> getBets() {
-        return bets;
-    }
-
     // Transforms the Adapter/HTML Form content to Domain Objects for us
     public List<PlayerBet> getPlayerBets() {
         List<PlayerBet> playerBets = new ArrayList<>();
@@ -57,33 +42,6 @@ public class BettingForm {
         return playerBets;
     }
 
-    public void setBets(List<Integer> bets) {
-        this.bets = bets;
-    }
-
-    @Override
-    public String toString() {
-        return "BettingForm[" +
-                "bets=" + bets + ']';
-    }
-
-    @Deprecated // the new HTML form uses the Map instead of this list
-    public List<Integer> getPlayerIds() {
-        return playerIds;
-    }
-
-    @Deprecated // the new HTML form uses the Map instead of this list
-    public void setPlayerIds(List<Integer> playerIds) {
-        playerIdToBets = new HashMap<>();
-        Iterator<Integer> betsIterator = bets.iterator();
-
-        for (Integer playerId : playerIds) {
-            playerIdToBets.put(String.valueOf(playerId), String.valueOf(betsIterator.next()));
-        }
-
-        this.playerIds = playerIds;
-    }
-
     public Map<String, String> getPlayerIdToBets() {
         return playerIdToBets;
     }
@@ -92,17 +50,5 @@ public class BettingForm {
         this.playerIdToBets = playerIdToBets;
     }
 
-    @Override
-    public boolean equals(Object obj) {
-        if (obj == this) return true;
-        if (obj == null || obj.getClass() != this.getClass()) return false;
-        var that = (BettingForm) obj;
-        return Objects.equals(this.bets, that.bets);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(bets);
-    }
 
 }
