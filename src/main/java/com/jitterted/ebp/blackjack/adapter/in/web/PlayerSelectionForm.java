4ba39d36366dc9d47428efd738bc721acb9d5f50
Ted M. Young <tedyoung@gmail.com>
2024-04-19 09:12:49 -0700

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:src/main/java/com/jitterted/ebp/blackjack/adapter/in/web/PlayerSelectionForm.java

--- src/main/java/com/jitterted/ebp/blackjack/adapter/in/web/PlayerSelectionForm.java
+++ src/main/java/com/jitterted/ebp/blackjack/adapter/in/web/PlayerSelectionForm.java
@@ -4,6 +4,9 @@ import java.util.List;
 
 public class PlayerSelectionForm {
 
+    // 1. Holds the players from which we can select to play the game
+    // 2. Accepts the players for which the checkbox was checked, i.e., they're playing
+
     public List<PlayerAccountView> getPlayers() {
         return null;
     }
