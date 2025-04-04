09987b9f8faf5ad7d7f35f637f5fa28bf8b4e4c6
Ted M. Young <tedyoung@gmail.com>
2025-04-04 09:09:48 -0700

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:src/test/java/com/jitterted/ebp/blackjack/HexArchTest.java

--- src/test/java/com/jitterted/ebp/blackjack/HexArchTest.java
+++ src/test/java/com/jitterted/ebp/blackjack/HexArchTest.java
@@ -21,78 +21,78 @@ import static com.tngtech.archunit.core.domain.properties.HasName.Predicates.nam
 
 @Tag("architecture")
 public class HexArchTest {
-
-    public static final ArchRule hexagonal_architecture =
-
-            Architectures.layeredArchitecture()
-                         .consideringOnlyDependenciesInLayers()
-                         .layer("Adapter").definedBy("..adapter..")
-                         .layer("Application").definedBy("..application..")
-                         .layer("Domain").definedBy("..domain..")
-                         .layer("Startup").definedBy("com.jitterted.ebp.blackjack")
-
-                         .whereLayer("Adapter").mayOnlyBeAccessedByLayers("Startup")
-                         .whereLayer("Application").mayOnlyBeAccessedByLayers("Adapter", "Startup")
-                         .whereLayer("Domain").mayOnlyBeAccessedByLayers("Application", "Adapter", "Startup");
-
-    @Test
-    void checkHexArch() {
-        JavaClasses importedClasses = new ClassFileImporter().importPackages("com.jitterted.ebp.blackjack");
-
-        hexagonal_architecture.check(importedClasses);
-    }
-
-    // this is the handwritten version, a simplified version is by ChatGPT below
-    @Test
-    void ensureAggregateReconstituteOnlyCalledByRepository() {
-        JavaClasses importedClasses = new ClassFileImporter().importPackages("com.jitterted.ebp.blackjack");
-
-        DescribedPredicate<? super JavaMethod> haveMethodAnnotatedWithReconstitute =
-                new DescribedPredicate<>("have a method annotated with @Reconstitute") {
-                    @Override
-                    public boolean test(JavaMethod method) {
-                        return method.isAnnotatedWith(Reconstitute.class);
-                    }
-                };
-        ArchCondition<? super JavaMethod> onlyBeAccessedByMethodsInRepositoryClass =
-                new ArchCondition<JavaMethod>("only be accessed by methods in *Repository") {
-                    @Override
-                    public void check(JavaMethod item, ConditionEvents events) {
-                        for (JavaCodeUnitAccess<?> methodCallToReconstitute : item.getAccessesToSelf()) {
-                            if (callerIsNotInTestClass(methodCallToReconstitute)) {
-                                if (!methodCallToReconstitute.getOrigin().getOwner().getFullName().endsWith("Repository")) {
-                                    events.add(SimpleConditionEvent.violated(item, "%s is called by method %s, but it is not in a Repository"
-                                            .formatted(item.getFullName(), methodCallToReconstitute.getOrigin().getFullName())));
-                                }
-                            }
-                        }
-                    }
-
-                    public boolean callerIsNotInTestClass(JavaCodeUnitAccess<?> callersToReconstitute) {
-                        return !callersToReconstitute.getOrigin().getOwner().getFullName().endsWith("Test")
-                                && !callersToReconstitute.getOrigin().getOwner().getEnclosingClass()
-                                                         .map(s -> s.getFullName().endsWith("Test")).orElse(false);
-                    }
-                };
-        ArchRule rule = ArchRuleDefinition.methods()
-                                          .that(haveMethodAnnotatedWithReconstitute)
-                                          .should(onlyBeAccessedByMethodsInRepositoryClass);
-
-        rule.check(importedClasses);
-    }
-
-    @Test
-    void ensureAggregateReconstituteOnlyCalledByRepositoryByChatGPT4() {
-        ArchRule rule = ArchRuleDefinition.methods()
-                                          .that().areAnnotatedWith(Reconstitute.class)
-                                          .should(ArchConditions.onlyBeCalledByClassesThat(nameEndingWith("Repository")))
-                                          .because("Methods annotated with @Reconstitute should only be accessed by Repository classes")
-                                          .allowEmptyShould(true);
-
-        rule.check(new ClassFileImporter()
-                           .withImportOption(ImportOption.Predefined.DO_NOT_INCLUDE_TESTS)
-                           .importPackages("com.jitterted.ebp.blackjack"));
-    }
-
+//
+//    public static final ArchRule hexagonal_architecture =
+//
+//            Architectures.layeredArchitecture()
+//                         .consideringOnlyDependenciesInLayers()
+//                         .layer("Adapter").definedBy("..adapter..")
+//                         .layer("Application").definedBy("..application..")
+//                         .layer("Domain").definedBy("..domain..")
+//                         .layer("Startup").definedBy("com.jitterted.ebp.blackjack")
+//
+//                         .whereLayer("Adapter").mayOnlyBeAccessedByLayers("Startup")
+//                         .whereLayer("Application").mayOnlyBeAccessedByLayers("Adapter", "Startup")
+//                         .whereLayer("Domain").mayOnlyBeAccessedByLayers("Application", "Adapter", "Startup");
+//
+//    @Test
+//    void checkHexArch() {
+//        JavaClasses importedClasses = new ClassFileImporter().importPackages("com.jitterted.ebp.blackjack");
+//
+//        hexagonal_architecture.check(importedClasses);
+//    }
+//
+//    // this is the handwritten version, a simplified version is by ChatGPT below
+//    @Test
+//    void ensureAggregateReconstituteOnlyCalledByRepository() {
+//        JavaClasses importedClasses = new ClassFileImporter().importPackages("com.jitterted.ebp.blackjack");
+//
+//        DescribedPredicate<? super JavaMethod> haveMethodAnnotatedWithReconstitute =
+//                new DescribedPredicate<>("have a method annotated with @Reconstitute") {
+//                    @Override
+//                    public boolean test(JavaMethod method) {
+//                        return method.isAnnotatedWith(Reconstitute.class);
+//                    }
+//                };
+//        ArchCondition<? super JavaMethod> onlyBeAccessedByMethodsInRepositoryClass =
+//                new ArchCondition<JavaMethod>("only be accessed by methods in *Repository") {
+//                    @Override
+//                    public void check(JavaMethod item, ConditionEvents events) {
+//                        for (JavaCodeUnitAccess<?> methodCallToReconstitute : item.getAccessesToSelf()) {
+//                            if (callerIsNotInTestClass(methodCallToReconstitute)) {
+//                                if (!methodCallToReconstitute.getOrigin().getOwner().getFullName().endsWith("Repository")) {
+//                                    events.add(SimpleConditionEvent.violated(item, "%s is called by method %s, but it is not in a Repository"
+//                                            .formatted(item.getFullName(), methodCallToReconstitute.getOrigin().getFullName())));
+//                                }
+//                            }
+//                        }
+//                    }
+//
+//                    public boolean callerIsNotInTestClass(JavaCodeUnitAccess<?> callersToReconstitute) {
+//                        return !callersToReconstitute.getOrigin().getOwner().getFullName().endsWith("Test")
+//                                && !callersToReconstitute.getOrigin().getOwner().getEnclosingClass()
+//                                                         .map(s -> s.getFullName().endsWith("Test")).orElse(false);
+//                    }
+//                };
+//        ArchRule rule = ArchRuleDefinition.methods()
+//                                          .that(haveMethodAnnotatedWithReconstitute)
+//                                          .should(onlyBeAccessedByMethodsInRepositoryClass);
+//
+//        rule.check(importedClasses);
+//    }
+//
+//    @Test
+//    void ensureAggregateReconstituteOnlyCalledByRepositoryByChatGPT4() {
+//        ArchRule rule = ArchRuleDefinition.methods()
+//                                          .that().areAnnotatedWith(Reconstitute.class)
+//                                          .should(ArchConditions.onlyBeCalledByClassesThat(nameEndingWith("Repository")))
+//                                          .because("Methods annotated with @Reconstitute should only be accessed by Repository classes")
+//                                          .allowEmptyShould(true);
+//
+//        rule.check(new ClassFileImporter()
+//                           .withImportOption(ImportOption.Predefined.DO_NOT_INCLUDE_TESTS)
+//                           .importPackages("com.jitterted.ebp.blackjack"));
+//    }
+//
 
 }
